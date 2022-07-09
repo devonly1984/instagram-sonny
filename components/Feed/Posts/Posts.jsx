84702edf27f855616506fbaadd66bb66830b99ee
John NoBody <devonly1984@hotmail.com>
2022-07-09 09:38:45 -0500

    Added firebase

--- components/Feed/Posts/Posts.jsx
+++ components/Feed/Posts/Posts.jsx
@@ -1,33 +1,35 @@
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { db } from "../../../firebase";
 import Post from "./Post";
-const posts = [
-  {
-    id: 123,
-    username: "bwhite",
-    userImg: "https://links.papareact.com/3ke",
-    img: "https://links.papareact.com/3ke",
-    caption: "Junk",
-  },
-  {
-    id: 456,
-    username: "bwhite",
-    userImg: "https://links.papareact.com/3ke",
-    img: "https://links.papareact.com/3ke",
-    caption: "Junk",
-  },
-];
+
 const Posts = () => {
+  const [posts, setPosts] = useState([]);
+  useEffect(
+    () =>
+      onSnapshot(
+        query(collection(db, "posts"), orderBy("timestamp", "desc")),
+        (snapshot) => {
+          setPosts(snapshot.docs);
+        }
+      ),
+
+    [db]
+  );
+
   return (
     <div>
-      {posts.map((post) => (
-        <Post
-          key={post.id}
-          id={post.id}
-          username={post.username}
-          userImg={post.userImg}
-          img={post.img}
-          caption={post.caption}
-        />
-      ))}
+      {posts &&
+        posts.map((post) => (
+          <Post
+            key={post.id}
+            id={post.id}
+            username={post.data.username}
+            profileImg={post.data.profileImg}
+            img={post.data.img}
+            caption={post.data.caption}
+          />
+        ))}
     </div>
   );
 };
