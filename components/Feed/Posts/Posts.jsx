e733d4bb4b8f0bd688b79c9e0d7a92d2e2e404cc
Bill White <soniclinuxknight@hotmail.com>
2022-07-11 01:07:10 -0500

    Fixed firebase bug

--- components/Feed/Posts/Posts.jsx
+++ components/Feed/Posts/Posts.jsx
@@ -1,37 +1,40 @@
 import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
 import { useEffect, useState } from "react";
-import { db } from "../../../firebase";
+
 import Post from "./Post";
+import { db } from "../../../firebase";
 
 const Posts = () => {
-  const [posts, setPosts] = useState([]);
-  useEffect(
-    () =>
-      onSnapshot(
-        query(collection(db, "posts"), orderBy("timestamp", "desc")),
-        (snapshot) => {
-          setPosts(snapshot.docs);
-        }
-      ),
+	const [posts, setPosts] = useState([]);
+	useEffect(
+		() =>
+			onSnapshot(
+				query(collection(db, "posts"), orderBy("timestamp", "desc")),
+				(snapshot) => {
+					setPosts(snapshot.docs);
+				}
+			),
 
-    [db]
-  );
+		[db]
+	);
 
-  return (
-    <div>
-      {posts &&
-        posts.map((post) => (
-          <Post
-            key={post.id}
-            id={post.id}
-            username={post.data.username}
-            profileImg={post.data.profileImg}
-            img={post.data.img}
-            caption={post.data.caption}
-          />
-        ))}
-    </div>
-  );
+	return (
+		<div>
+			{posts &&
+				posts.map((post) => {
+					return (
+						<Post
+							key={post.id}
+							id={post.data().id}
+							username={post.data().username}
+							caption={post.data().caption}
+							profileImg={post.data().profileImg}
+							img={post.data().image}
+						/>
+					);
+				})}
+		</div>
+	);
 };
 
 export default Posts;
